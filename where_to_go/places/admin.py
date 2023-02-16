1a6dbc407f00dda10807b6ce66faf11d728facd6
Ivan Dorofeev <axxel123@rambler.ru>
2023-02-16 09:54:24 +0300

    correct mistakes

--- where_to_go/places/admin.py
+++ where_to_go/places/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+
 from .models import Location, Image
 from adminsortable2.admin import SortableInlineAdminMixin
 
 
 class PlaceImageInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Image
-    readonly_fields = ['image', 'image_preview', ]
-    fields = ['location', 'image']
+    readonly_fields = ['image_preview']
+    fields = ['location', 'image', 'image_preview', 'index']
 
-    def image_preview(self, obj):
-        return obj
+    def image_preview(self, model):
+        try:
+            preview = mark_safe('<img src="{url}" width="{width}" height={height} />'.format(
+                url=model.image.url,
+                width=400,
+                height=200,
+            )
+            )
+            return preview
+        except Exception as exc:
+            print(Exception, exc)
 
 
 @admin.register(Location)
@@ -23,4 +34,4 @@ class LocationAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImagesAdmin(admin.ModelAdmin):
-    fields = ('location', 'image')
+    fields = ('location', 'image', 'index')
--- where_to_go/places/management/commands/load_places.py
+++ where_to_go/places/management/commands/load_places.py
@@ -26,28 +26,42 @@ def prepare_images(new_images_paths):
 
 class Command(BaseCommand):
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-p',
+            '--path',
+            nargs='+',
+            type=str,
+            help='Указывается путь откуда будут будут браться JSON файлы. По умолчанию из places_and_pictures/places/'
+        )
+
     def handle(self, *args, **options):
         base_dir = BASE_DIR.resolve().parent
         new_places_jsons_path = os.path.join(base_dir, 'places_and_pictures/places/')
+        if options['path']:
+            try:
+                new_places_jsons_path = os.path.join(base_dir, str(options['path'][0]))
+            except Exception as exc:
+                print('Проблемы с указанием пути для JSON файла: ', exc)
 
         for address, dirs, files in os.walk(new_places_jsons_path):
             for json_file in files:
                 json_file_path = os.path.join(address, json_file)
                 with open(json_file_path, 'r') as json_file:
-                    readed_json_file = json.load(json_file)
-                    readed_images = readed_json_file['imgs']
+                    read_json_file = json.load(json_file)
+                    read_images = read_json_file['imgs']
                 try:
                     place, created = Location.objects.get_or_create(
-                        title=readed_json_file['title'],
+                        title=read_json_file['title'],
                         defaults={
-                            'short_description': readed_json_file['description_short'],
-                            'long_description': readed_json_file['description_long'],
-                            'latitude': readed_json_file['coordinates']['lng'],
-                            'longtitude': readed_json_file['coordinates']['lat']
+                            'short_description': read_json_file['description_short'],
+                            'long_description': read_json_file['description_long'],
+                            'latitude': read_json_file['coordinates']['lng'],
+                            'longtitude': read_json_file['coordinates']['lat']
                         }
                     )
 
-                    download_images = prepare_images(readed_images)
+                    download_images = prepare_images(read_images)
                     for download_image in download_images:
                         img_file_name = download_image[0]
                         img_content = ContentFile(download_image[1])
@@ -59,4 +73,4 @@ class Command(BaseCommand):
                         created_image_model[0].image.save(img_file_name, img_content, save=True)
 
                 except Location.DoesNotExist:
-                    raise CommandError('Location cant find - "%s"' % readed_json_file['title'])
+                    raise CommandError('Location cant find - "%s"' % read_json_file['title'])
